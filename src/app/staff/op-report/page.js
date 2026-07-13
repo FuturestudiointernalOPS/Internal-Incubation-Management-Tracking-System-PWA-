b7037b906e54cca643d38f66af1fb788e35054d2
chrisnaudmorgane <chrisnaud.agossou@epitech.eu>
2026-07-13 14:26:02 +0100

    feat: add PM submission API endpoint and implement staff operational report interface

--- src/app/staff/op-report/page.js
+++ src/app/staff/op-report/page.js
@@ -29,6 +29,7 @@ import {
 import { useRouter } from "next/navigation";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import { useI18n } from "@/lib/i18n";
+import TaskManager from "@/components/tasks/TaskManager";
 import TaskDetailModal from "@/components/ui/TaskDetailModal";
 
 /**
